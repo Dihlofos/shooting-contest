86704c2902b809be7bd24382bcba50a870ec43db
Samsonov Egeny <evgeniy.samsonov@sbermegamarket.ru>
2024-03-25 19:32:33 +0400

    add go to anchor on load

--- build/js/main3.js
+++ build/js/main3.js
@@ -236,6 +236,15 @@ $(document).ready(function () {
     speedAsDuration: true,
     easing: "easeOutQuad",
   });
+
+  const id = window.location.hash.substring(1); //Puts hash in variable, and removes the # character
+
+  console.log("id", id);
+  const targetElement = document.querySelector(`#${id}`);
+
+  if (targetElement) {
+    targetElement.scrollIntoView({ behavior: "smooth" });
+  }
 })();
 
 "use strict";
--- source/js/main/smooth-scroll.js
+++ source/js/main/smooth-scroll.js
@@ -5,4 +5,13 @@
     speedAsDuration: true,
     easing: "easeOutQuad",
   });
+
+  const id = window.location.hash.substring(1); //Puts hash in variable, and removes the # character
+
+  console.log("id", id);
+  const targetElement = document.querySelector(`#${id}`);
+
+  if (targetElement) {
+    targetElement.scrollIntoView({ behavior: "smooth" });
+  }
 })();
