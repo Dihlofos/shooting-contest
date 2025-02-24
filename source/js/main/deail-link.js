e3850805e6262bfb09cb535e6fad0dcca3524123
Samsonov Egeny <evgeniy.samsonov@sbermegamarket.ru>
2025-02-24 17:58:32 +0300

    fix

--- source/js/main/deail-link.js
+++ source/js/main/deail-link.js
@@ -13,8 +13,6 @@
       method: "GET",
     });
 
-    console.log("response", response);
-
     if (response.url !== "https://shotgun.sport.moscow/404.html") {
       link.classList.remove("disabled");
     }
